2bb93a2016c9e9b5b2cf3b3e583fa713a246833f
Patrick <patrick.andrew.taylor@gmail.com>
2019-09-14 17:57:18 -0400

    Generalizing for multiple countries.

--- flags-mashup.py
+++ flags-mashup.py
@@ -4,18 +4,25 @@ from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
 from PIL import Image
 
+COUNTRY_CODES = {
+    'India': 'IN',
+    'United States': 'US'
+}
 
-def main():
-    india_flag_remote = rapi.get_country_by_country_code('IN').flag
-    india_flag_local_svg = 'tmp/india.svg'
-    india_flag_local_png = 'tmp/india.png'
-    urllib.request.urlretrieve(india_flag_remote, india_flag_local_svg)
-    renderPM.drawToFile(svg2rlg(india_flag_local_svg), india_flag_local_png, fmt='PNG')
-    india_flag_pil = Image.open(india_flag_local_png)
-    india_flag_pil_horz = india_flag_pil.size[0]
-    india_flag_pil_vert = india_flag_pil.size[1]
-    india_flag_colors = india_flag_pil.getcolors(india_flag_pil_horz*india_flag_pil_vert)
-    return india_flag_colors
+
+def country_flag_colors(country):
+    flag_remote = rapi.get_country_by_country_code(COUNTRY_CODES.get(country.capitalize())).flag
+    flag_local_svg = 'tmp/{}.svg'.format(country.lower())
+    flag_local_png = 'tmp/{}.png'.format(country.lower())
+    urllib.request.urlretrieve(flag_remote, flag_local_svg)
+    renderPM.drawToFile(svg2rlg(flag_local_svg), flag_local_png, fmt='PNG')
+    flag_pil = Image.open(flag_local_png)
+    flag_pil_horz = flag_pil.size[0]
+    flag_pil_vert = flag_pil.size[1]
+    flag_colors = flag_pil.getcolors(flag_pil_horz*flag_pil_vert)
+    return flag_colors
+
 
 if __name__ == '__main__':
-    main()
+    india_flag_colors = country_flag_colors('India')
+    us_flag_colors = country_flag_colors('United States')
