7ac8f148f1d4c50dfe19ff79cd17c21fb5bb051e
Patrick <patrick.andrew.taylor@gmail.com>
2019-09-14 19:10:46 -0400

    Moving tmp dir creation.

--- flags-mashup.py
+++ flags-mashup.py
@@ -16,6 +16,8 @@ COLOR_GET = namedtuple('COLOR_GET', 'count RGB')
 
 
 def country_flag(country):
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
     country_code = COUNTRY_CODES.get(country.title())
     flag_remote = rapi.get_country_by_country_code(COUNTRY_CODES.get(country.title())).flag
     flag_local_svg = 'tmp/{}.svg'.format(country_code)
@@ -36,8 +38,6 @@ def country_flag_colors(flag_pil):
 def main():
     if not os.path.exists('swaps'):
         os.makedirs('swaps')
-    if not os.path.exists('tmp'):
-        os.makedirs('tmp')
     country_one = 'India'
     country_one_code = COUNTRY_CODES.get(country_one.title())
     country_one_flag = country_flag(country_one)
