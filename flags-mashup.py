3ad32dd2897892c0f513b48a7f9c3eda9b7e5a4a
Patrick <patrick.andrew.taylor@gmail.com>
2019-09-14 17:26:12 -0400

    Updating local directory. Removing reference to US.

--- flags-mashup.py
+++ flags-mashup.py
@@ -3,9 +3,9 @@ import urllib.request
 
 
 def main():
-    # Grab India & US Flags
+    # Grab India Flag
     india_flag_remote = rapi.get_country_by_country_code('IN').flag
-    india_flag_local = '/tmp/india.svg'
+    india_flag_local = 'tmp/india.svg'
     urllib.request.urlretrieve(india_flag_remote, india_flag_local)
 
 
