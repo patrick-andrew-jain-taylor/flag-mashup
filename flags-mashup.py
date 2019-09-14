84639a41e2b8d5f483897c8714d6c7be7d544978
Patrick <patrick.andrew.taylor@gmail.com>
2019-09-14 17:29:36 -0400

    Removing Pillow from requirements.

--- flags-mashup.py
+++ flags-mashup.py
@@ -1,7 +1,6 @@
 from restcountries import RestCountryApiV2 as rapi
 import urllib.request
 
-
 def main():
     # Grab India Flag
     india_flag_remote = rapi.get_country_by_country_code('IN').flag
