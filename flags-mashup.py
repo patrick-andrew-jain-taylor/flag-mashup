215401104f4a930585bf9b60f585f62e1d3a3318
Patrick <patrick.andrew.taylor@gmail.com>
2019-09-14 17:39:35 -0400

    Converting SVG to PNG for PIL usage.

--- flags-mashup.py
+++ flags-mashup.py
@@ -1,11 +1,16 @@
 from restcountries import RestCountryApiV2 as rapi
 import urllib.request
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+
 
 def main():
     # Grab India Flag
     india_flag_remote = rapi.get_country_by_country_code('IN').flag
-    india_flag_local = 'tmp/india.svg'
-    urllib.request.urlretrieve(india_flag_remote, india_flag_local)
+    india_flag_local_svg = 'tmp/india.svg'
+    india_flag_local_png = 'tmp/india.png'
+    urllib.request.urlretrieve(india_flag_remote, india_flag_local_svg)
+    renderPM.drawToFile(svg2rlg(india_flag_local_svg), india_flag_local_png, fmt='PNG')
 
 
 if __name__ == '__main__':
