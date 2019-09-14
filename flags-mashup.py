64274c61b3d3c58be5a6bc93bbd6379ce09bdf61
Patrick <patrick.andrew.taylor@gmail.com>
2019-09-14 17:45:18 -0400

    Open image with PIL.

--- flags-mashup.py
+++ flags-mashup.py
@@ -2,6 +2,7 @@ from restcountries import RestCountryApiV2 as rapi
 import urllib.request
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
+from PIL import Image
 
 
 def main():
@@ -11,7 +12,8 @@ def main():
     india_flag_local_png = 'tmp/india.png'
     urllib.request.urlretrieve(india_flag_remote, india_flag_local_svg)
     renderPM.drawToFile(svg2rlg(india_flag_local_svg), india_flag_local_png, fmt='PNG')
-
+    india_flag_pil = Image.open(india_flag_local_png)
+    print(india_flag_pil)
 
 if __name__ == '__main__':
     main()
